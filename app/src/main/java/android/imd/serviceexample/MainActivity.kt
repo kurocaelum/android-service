acfbee246257565ce0e731c3f509fdb100ba8b14
Victor <victor_brandao@outlook.com>
2019-10-14 20:10:50 -0300

    Update

--- app/src/main/java/android/imd/serviceexample/MainActivity.kt
+++ app/src/main/java/android/imd/serviceexample/MainActivity.kt
@@ -1,13 +1,18 @@
 package android.imd.serviceexample
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var i: Intent? = null
+    private var service: TimeService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,14 @@ class MainActivity : AppCompatActivity() {
     fun start(){
         i = Intent(applicationContext, TimeService::class.java)
         startService(i)
+
+        val conn = TimeServiceConnection()
+        bindService(i, conn, 0)
+    }
+
+    fun read(){
+        val second = service?.getSeconds()
+        txt_seconds.text = second.toString()
     }
 
     fun stop(){
@@ -24,7 +37,15 @@ class MainActivity : AppCompatActivity() {
             stopService(i)
     }
 
-    fun read(){
+    inner class TimeServiceConnection: ServiceConnection{
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onServiceConnected(p0: ComponentName?, iBinder: IBinder?) {
+            var binder: TimeService.TimeServiceBinder = iBinder as TimeService.TimeServiceBinder
+            this@MainActivity.service = binder.service
+        }
 
     }
 }
